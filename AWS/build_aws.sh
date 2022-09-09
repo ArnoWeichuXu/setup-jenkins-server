70321267ffa400f8956f77c16dc9a30f671d739c
ArnoWeichuXu <arno.xu.weichu@gmail.com>
2022-09-09 14:18:58 +1000

    update scripts

--- AWS/build_aws.sh
+++ AWS/build_aws.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd terraform/
 terraform init && terraform fmt -recursive && terraform apply --auto-approve
--- AWS/destroy_aws.sh
+++ AWS/destroy_aws.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd terraform/
 terraform destroy --auto-approve
